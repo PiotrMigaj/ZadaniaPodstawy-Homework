5d22a7f8201cc957745215826c8defdb3f874ef5
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-04-05 07:47:03 +0200

    solution of ex11 day1

---
+++ src/day1/ex11/Day1Ex11Main.java
@@ -0,0 +1,39 @@
+package day1.ex11;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day1Ex11Main {
+
+    private static final int RANDOM_NUMBER = new Random().nextInt(100) + 1;
+
+    public static void main(String[] args) {
+        runMyGame();
+    }
+
+    public static void runMyGame(){
+
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        do {
+            System.out.println("Enter integer number:");
+            input = scanner.nextInt();
+            scanner.nextLine();
+
+            if (input==RANDOM_NUMBER){
+                System.out.println("Bingo");
+                break;
+            }
+            if (input>RANDOM_NUMBER){
+                System.out.println("too much");
+            }
+            if (input<RANDOM_NUMBER){
+                System.out.println("not enough");
+            }
+
+        }while (input!=RANDOM_NUMBER);
+
+    }
+
+}
